c924ef7757f12dbeaa6321e81d741acef17060e9
karamo10 <pabimamie@gmail.com>
2025-08-15 10:25:12 +0000

    update: new components, UI improvements, Cloudinary configuration changes, and ESLint error fixes

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,37 +1,44 @@
-import { NextRequest, NextResponse } from "next/server";
-import cloudinary from "@/libs/cloudinary";
-import { UploadApiResponse } from "cloudinary";
+import { NextRequest, NextResponse } from 'next/server';
+import cloudinary from '@/libs/cloudinary';
+import { UploadApiResponse } from 'cloudinary';
 
 export async function POST(req: NextRequest) {
-    try { 
-        const formData = await req.formData();
-        const file = formData.get('file') as File;
+  try {
+    const formData = await req.formData();
+    const file = formData.get('file') as File;
 
-        if (!file) {
-            return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
-        }
-
-        const arrayBuffer = await file.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-
-        const uploadResult = await new Promise<UploadApiResponse>((resolve, reject) => {
-            const stream = cloudinary.uploader.upload_stream(
-                {
-                    folder: "test_cloud_upload",
-                    upload_preset: "test_cloud"
-                 },
-                (error, result) => {
-                    if (error) reject(error);
-                    else if (result) resolve(result);
-                    else reject(new Error("Upload result is undefined"));
-                }
-            );
-            stream.end(buffer);
-        });
-
-        return NextResponse.json({ secure_url: uploadResult.secure_url });
+    if (!file) {
+      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
     }
-    catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise<UploadApiResponse>(
+      (resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          {
+            folder: 'test_cloud_upload',
+            upload_preset: 'test_cloud',
+          },
+          (error, result) => {
+            if (error) reject(error);
+            else if (result) resolve(result);
+            else reject(new Error('Upload result is undefined'));
+          }
+        );
+        stream.end(buffer);
+      }
+    );
+
+    return NextResponse.json({ secure_url: uploadResult.secure_url });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
     }
+    return NextResponse.json(
+      { error: 'An unknown error occurred' },
+      { status: 500 }
+    );
+  }
 }
