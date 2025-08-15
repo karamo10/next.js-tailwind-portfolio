44ddef2fb4526868bae37f406ed5312b02c6a8a1
karamo10 <pabimamie@gmail.com>
2025-08-15 12:27:29 +0000

    fix: update /blogs/[slug]/edit/page.tsx to fix PageProps type error and correctly type async param

--- app/blogs/[slug]/edit/page.tsx
+++ app/blogs/[slug]/edit/page.tsx
@@ -2,15 +2,16 @@ import EditBlogPost from '@/app/ui/blog/edit-blog-post';
 import { fetchClientBySlug } from '@/libs/data';
 import { Client } from '@/libs/definitions';
 
-interface EditPageParams {
+type PageProps = {
   params: { slug: string };
-}
+  searchParams?: Record<string, string | string[] | undefined>;
+};
 
-export default async function EditPage({ params }: EditPageParams) {
+export default async function EditPage({ params }: PageProps) {
   const posts: Client[] = await fetchClientBySlug(params.slug);
-  // const posts = await fetchClientBySlug(params.slug);
-
+  
   const post = posts[0];
-
+  
   return <EditBlogPost post={post} />;
 }
+// const posts = await fetchClientBySlug(params.slug);
